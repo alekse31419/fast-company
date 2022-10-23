2b499a07d5e0f61119bec6cb6dd2c4b2cf7258bb
Alexey Vertsanov <alekse31419@gmail.com>
2022-10-23 23:15:31 +0600

    delete table if zero

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -36,34 +36,36 @@ const Users = () => {
   return (
     <div className="p-3">
       <h2>{renderPhrase(users.length)}</h2>
-      <table className="table align-middle mt-4">
-        <thead>
-          <tr>
-            <th>Имя</th>
-            <th>Качества</th>
-            <th>Профессия</th>
-            <th>Встретился раз</th>
-            <th>Оценка</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            users.map(user => {
-              return (
-                <tr key={user._id}>
-                  <td>{user.name}</td>
-                  <td>{renderQualities(user)}</td>
-                  <td>{user.profession.name}</td>
-                  <td>{user.completedMeetings}</td>
-                  <td>{user.rate}</td>
-                  <td className="text-center"><button className="btn btn-danger" onClick={() => handleDelete(user._id)}>Удалить</button></td>
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
+      {users.length ? (
+        <table className="table align-middle mt-4">
+          <thead>
+            <tr>
+              <th>Имя</th>
+              <th>Качества</th>
+              <th>Профессия</th>
+              <th>Встретился раз</th>
+              <th>Оценка</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map(user => {
+                return (
+                  <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>{renderQualities(user)}</td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate}</td>
+                    <td className="text-center"><button className="btn btn-danger" onClick={() => handleDelete(user._id)}>Удалить</button></td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      ) : ''}
     </div>
   )
 }
